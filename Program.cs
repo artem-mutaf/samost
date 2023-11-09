7ec9fded53b625583dd67c4ef0f8905709052239
artem-mutaf <123882882+artem-mutaf@users.noreply.github.com>
2023-11-09 11:18:15 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace samostoialka
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = int.Parse(Console.ReadLine());
+            Console.Write("Введите в какую степень хотите возвести ");
+            int step = 3;
+            Console.WriteLine($"Число {x} в степени {step} = " + Math.Pow(x,step));
+            Console.ReadKey();
+
+        }
+    }
+}
